476568c3c3cfccd474786f43e7c1be4ad7c51694
郑斌斌 <506745710@qq.com>
2021-10-22 19:44:40 +0800

    interface build

---
+++ src/pages/download/index.js
@@ -0,0 +1,25 @@
+import './index.scss'
+import { useEffect } from 'react'
+
+export default function Download() {
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <div className="DfinityPage">
+      <div className="content">
+        <div className="card Extension">
+
+        </div>
+        <div className="card API">
+
+        </div>
+        <div className="card APP">
+
+        </div>
+      </div>
+    </div>
+  )
+}
