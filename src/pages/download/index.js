b1bf4547b766030630192ad0e02c7f821c07a4fc
郑斌斌 <506745710@qq.com>
2021-10-23 16:41:28 +0800

    新增Access应用

--- src/pages/download/index.js
+++ src/pages/download/index.js
@@ -4,6 +4,7 @@ import motokoImg from '../../static/img/motoko.svg'
 import EthereumImg from '../../static/img/Ethereum_icon.png'
 import ReversiImg from '../../static/img/Reversi.png'
 import MagniImg from '../../static/img/Magni.png'
+import ICTexasImg from '../../static/img/ICTexas.png'
 
 
 export default function Download() {
@@ -18,6 +19,8 @@ export default function Download() {
       window.open('https://ivg37-qiaaa-aaaab-aaaga-cai.ic0.app/#!/play');
     } else if (value == 'Rise') {
       window.open('https://riseofthemagni.com/');
+    } else if (value == 'Texas') {
+      window.open('https://lm5fh-ayaaa-aaaah-aafua-cai.ic0.app/#/');
     } else if (value == 'Ethereum') {
       window.open('https://github.com/relationlabs/rel-contract');
     } else if (value == 'Motoko') {
@@ -29,32 +32,6 @@ export default function Download() {
   return (
     <div className="DfinityPage">
       <div className="content">
-        <div className="card APP">
-          <p className="cardName">Access Dapp </p>
-          <div className="list">
-            <div className="item" onClick={downloadFile.bind(this,'Reversi')}>
-              <img src={ReversiImg}></img>
-              <span>Reversi</span>
-            </div>
-            <div className="item" onClick={downloadFile.bind(this,'Rise')}>
-              <img src={MagniImg}></img>
-              <span>Rise Of The</span>
-            </div>
-          </div>
-        </div>
-        <div className="card API">
-          <p className="cardName">Project Download</p>
-          <div className="list">
-            <div className="item" onClick={downloadFile.bind(this,'Ethereum')}>
-              <img src={EthereumImg}></img>
-              <span>Ethereum合约项目地址</span>
-            </div>
-            <div className="item" onClick={downloadFile.bind(this,'Motoko')}>
-              <img src={motokoImg}></img>
-              <span>Motoko合约项目地址</span>
-            </div>
-          </div>
-        </div>
         <div className="card Extension">
           <p className="cardName">Extension Download</p>
           <div className="list">
@@ -72,7 +49,37 @@ export default function Download() {
             </div>
           </div>
         </div>
+        <div className="card API">
+          <p className="cardName">Project Download</p>
+          <div className="list">
+            <div className="item" onClick={downloadFile.bind(this,'Ethereum')}>
+              <img src={EthereumImg}></img>
+              <span>Ethereum合约项目地址</span>
+            </div>
+            <div className="item" onClick={downloadFile.bind(this,'Motoko')}>
+              <img src={motokoImg}></img>
+              <span>Motoko合约项目地址</span>
+            </div>
+          </div>
         </div>
+        <div className="card APP">
+          <p className="cardName">Access Dapp </p>
+          <div className="list">
+            <div className="item" onClick={downloadFile.bind(this,'Reversi')}>
+              <img src={ReversiImg}></img>
+              <span>Reversi</span>
+            </div>
+            <div className="item" onClick={downloadFile.bind(this,'Texas')}>
+              <img src={ICTexasImg}></img>
+              <span>Texas Hold'em</span>
+            </div>
+            <div className="item" onClick={downloadFile.bind(this,'Rise')}>
+              <img src={MagniImg}></img>
+              <span>Rise of the Magni</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
